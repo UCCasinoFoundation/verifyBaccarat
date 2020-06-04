0656eff049cc2a61a5a9c1b30942b663ff534786
uccasino <66396998+uccasino@users.noreply.github.com>
2020-06-04 11:08:10 +0900

    Create verify.js

---
+++ verify.js
@@ -0,0 +1,6 @@
+function verifyBaccarat(hash, salt): gameResult {
+      const hashList = makeHashList(hash, salt);
+      const eight-deck-shoe = makeShoe(hashList);
+      const gameResult = simulateBaccarat(eight-deck-shoe);
+      return gameResult;
+}
